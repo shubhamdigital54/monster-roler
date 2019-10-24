6fef688c851e9d54680d095906892e3df2378c06
shubhamdigital54 <shubhamdigital54@gmail.com>
2019-10-24 21:57:37 +0530

    Updates

---
+++ precache-manifest.fb19a0d188107827c0b75f7474f95bc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3905a290e454952af96b91887f13f294",
+    "url": "/monster-roler/index.html"
+  },
+  {
+    "revision": "f597de8953f21115d9bb",
+    "url": "/monster-roler/static/css/main.167b2155.chunk.css"
+  },
+  {
+    "revision": "7b01ed5d9ae779e40b96",
+    "url": "/monster-roler/static/js/2.cdf771b7.chunk.js"
+  },
+  {
+    "revision": "f597de8953f21115d9bb",
+    "url": "/monster-roler/static/js/main.2330f206.chunk.js"
+  },
+  {
+    "revision": "d7f9fc5e9da6e9c98563",
+    "url": "/monster-roler/static/js/runtime-main.3458b3cd.js"
+  }
+]);
